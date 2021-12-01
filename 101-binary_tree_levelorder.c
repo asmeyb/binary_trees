450267e6d5e33aee078fe2baf3f21f3afc3b0e62
asmeyb <asmeb@gmail.com>
2021-12-01 11:53:55 -0800

    tree to traverse

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,77 +1,122 @@
 #include "binary_trees.h"
 
+levelorder_queue_t *create_node(binary_tree_t *node);
+void free_queue(levelorder_queue_t *head);
+void pint_push(binary_tree_t *node, levelorder_queue_t *head,
+		levelorder_queue_t **tail, void (*func)(int));
+void pop(levelorder_queue_t **head);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+
 /**
- * binary_tree_levelorder - level order
- * @tree: tree
- * @func: func
- * Return: void
+ * create_node - Creates a new levelorder_queue_t node.
+ * @node: The binary tree node for the new node to contain.
+ *
+ * Return: If an error occurs, NULL.
+ *         Otherwise, a pointer to the new node.
+ */
+levelorder_queue_t *create_node(binary_tree_t *node)
+{
+	levelorder_queue_t *new;
+
+	new = malloc(sizeof(levelorder_queue_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->node = node;
+	new->next = NULL;
+
+	return (new);
+}
+
+/**
+ * free_queue - Frees a levelorder_queue_t queue.
+ * @head: A pointer to the head of the queue.
+ */
+void free_queue(levelorder_queue_t *head)
+{
+	levelorder_queue_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
+
+/**
+ * pint_push - Runs a function on a given binary tree node and
+ *             pushes its children into a levelorder_queue_t queue.
+ * @node: The binary tree node to print and push.
+ * @head: A double pointer to the head of the queue.
+ * @tail: A double pointer to the tail of the queue.
+ * @func: A pointer to the function to call on @node.
+ *
+ * Description: Upon malloc failure, exits with a status code of 1.
+ */
+void pint_push(binary_tree_t *node, levelorder_queue_t *head,
+		levelorder_queue_t **tail, void (*func)(int))
+{
+	levelorder_queue_t *new;
+
+	func(node->n);
+	if (node->left != NULL)
+	{
+		new = create_node(node->left);
+		if (new == NULL)
+		{
+			free_queue(head);
+			exit(1);
+		}
+		(*tail)->next = new;
+		*tail = new;
+	}
+	if (node->right != NULL)
+	{
+		new = create_node(node->right);
+		if (new == NULL)
+		{
+			free_queue(head);
+			exit(1);
+		}
+		(*tail)->next = new;
+		*tail = new;
+	}
+}
+
+/**
+ * pop - Pops the head of a levelorder_queue_t queue.
+ * @head: A double pointer to the head of the queue.
+ */
+void pop(levelorder_queue_t **head)
+{
+	levelorder_queue_t *tmp;
+
+	tmp = (*head)->next;
+	free(*head);
+	*head = tmp;
+}
+
+/**
+ * binary_tree_levelorder - Traverses a binary tree using
+ *                          level-order traversal.
+ * @tree: A pointer to the root node of the tree to traverse.
+ * @func: A pointer to a function to call for each node.
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	size_t i, height;
+	levelorder_queue_t *head, *tail;
 
 	if (tree == NULL || func == NULL)
 		return;
-	height = binary_tree_height(tree);
-	for (i = 0; i <= height; i++)
-		print_tree(tree, func, i);
-}
-/**
- * print_tree - prints
- * @tree: tree
- * @func: func
- * @level: depth
- */
-void print_tree(const binary_tree_t *tree, void (*func)(int), size_t level)
-{
-	if (level == 0)
-		func(tree->n);
-	else
+
+	head = tail = create_node((binary_tree_t *)tree);
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
 	{
-		print_tree(tree->left, func, level - 1);
-		print_tree(tree->right, func, level - 1);
+		pint_push(head->node, head, &tail, func);
+		pop(&head);
 	}
-}
-/**
- * binary_tree_height - height of a binary tree
- * @tree: input tree
- * Return: height of tree
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (measure(tree) - 1);
-}
-/**
- * measure - helper
- * @tree: input tree
- * Return: height of tree
- */
-size_t measure(const binary_tree_t *tree)
-{
-	size_t left, right;
-	int total;
-
-	if (tree == NULL)
-		return (0);
-
-	left = measure(tree->left);
-	right = measure(tree->right);
-	total = max(left, right) + 1;
-	return (total);
-}
-
-/**
- * max - helper
- * @a: the first number
- * @b: the second number
- * Return: height of tree
- */
-int max(int a, int b)
-{
-	if (a > b)
-		return (a);
-	else
-		return (b);
 }
